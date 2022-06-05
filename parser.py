a1a6c7b32f24f41e9048e6b3fcf478a6d5dc1d15
nbrahmani <brahmani3110@gmail.com>
2022-06-05 21:03:31 +0530

    Minor bug fixes

--- parser.py
+++ parser.py
@@ -40,7 +40,6 @@ def main(parser, input_file, out_dir, candc_bin_dir, candc_models_dir, ccg2lambd
                          parser,
                          f"{file_prefix}.tok.preprocess.log")
     elif parser == "depccg":
-        import ipdb; ipdb.set_trace()
         print("Tokenizing...")
         file_prefix = input_file.split(".")[0]
         out_file = os.path.join(out_dir, file_prefix + ".tok")
@@ -49,7 +48,6 @@ def main(parser, input_file, out_dir, candc_bin_dir, candc_models_dir, ccg2lambd
         else:
             # (f"sed -f {ccg2lambda_dir}/en/tokenizer.sed {input_file} > {out_file}", shell=True)
             run(f"cat {input_file} | {ccg2lambda_dir}/en/tokenizer.sed | perl -pe 's/ \n/\n/g; s/ \.//g; s/ ,//g; s/\(/-LRB-/g; s/\)/-RRB-/g' > {file_prefix}.tok", shell=True)
-
 
         # from preprocess import preprocess
         # produces {file_prefix}.tok.clean
@@ -63,12 +61,16 @@ def main(parser, input_file, out_dir, candc_bin_dir, candc_models_dir, ccg2lambd
         if Path(parsed_file).exists():
             print("Parsed file already exists")
         else:
+            """
             print("Parsing with depccg...")
             from depccg import __main__
             from depccg.argparse import parse_args
-            sys.argv = [sys.argv[0], 'en', '--model', 'elmo', '-f', 'auto_extended', '-a', 'spacy']
+            sys.argv = [sys.argv[0], 'en', '--model', 'elmo', '-f',
+                        'auto_extended', '-a', 'spacy', '-i',
+                        f'{file_prefix}.tok.clean']
             parse_args(__main__.main)
-            # run(f"cat {file_prefix}.tok.clean | python3 -m depccg en --model elmo -f auto_extended -a spacy > {file_prefix}.depccg.parsed.txt", shell=True)
+            """
+            run(f"cat {file_prefix}.tok.clean | python3 -m depccg en --model elmo -f auto_extended -a spacy > {file_prefix}.depccg.parsed.txt", shell=True)
 
         run(f"sed -i 's/(</{{</g; s/>)/>}}/g; s/ )/ }}/g' {file_prefix}.depccg.parsed.txt", shell=True)
 
